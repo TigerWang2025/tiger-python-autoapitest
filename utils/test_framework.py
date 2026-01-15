3a48a9d20ceb06aaf6b80c47003b1506f158dbae
TigerWang2025 <tiger.wangmin@hotmail.com>
2026-01-15 22:06:28 +0800

    修改提交

--- utils/test_framework.py
+++ utils/test_framework.py
@@ -6,26 +6,29 @@ import jsonpath
 from xToolkit import xfile
 from string import Template
 
-datas = xfile.read(r"D:\CodeSpace\tiger-python-autoapitest\resource\Test.xls").excel_to_dict(sheet=1)
-
+datas = xfile.read(r"E:\CodeSpace\tiger-python-autoapitest\resource\Test.xls").excel_to_dict(sheet=1)
 
 # print(datas)
 
 dic = {}
+
+
 @pytest.mark.parametrize("case_info", datas)  # 和for循环一样，循环数据
 def test_excute(case_info):
     url = Template(case_info["接口URL"]).substitute(dic)
-    应答 = requests.request(url=url,
-                            method=case_info["请求方式"],
-                            params=eval(case_info["URL参数"]),
-                            data=eval(case_info["JSON参数"]))
-    print(应答.json())
+    result = requests.request(url=url,
+                              method=case_info["请求方式"],
+                              params=eval(case_info["URL参数"]),
+                              data=eval(case_info["JSON参数"]))
+    print(result.json())
 
-    assert 应答.status_code == 200
+    assert result.status_code == 200
 
     if case_info["提取参数"]:
-        token = jsonpath.jsonpath(应答.json(), "$.." + case_info["提取参数"])
+        token = jsonpath.jsonpath(result.json(), "$.." + case_info["提取参数"])
         dic[case_info["提取参数"]] = token[0]
+
+
 # for case_info in datas:
 #     excute(case_info)
 
@@ -37,9 +40,13 @@ def test_excute(case_info):
 
 
 if __name__ == '__main__':
-    pytest.main(["-vs",
-                 "--capture=sys",  # 控制台输出
-                 "test_framework.py",
-                 "--alluredir=allure-result"
-                 ])
+    # 运行 pytest 并生成 allure 结果
+    pytest.main([
+        "-vs",
+        "--capture=sys",
+        "test_framework.py",
+        "--alluredir=allure-result"
+    ])
+
+    # 生成 allure 报告
     os.system("allure generate allure-result -o ./report-allure")
